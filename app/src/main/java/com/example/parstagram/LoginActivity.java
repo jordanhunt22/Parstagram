246422bb0099b069aad216b8fcc1d564117e9872
Jordan Hunt <jordanhunt@fb.com>
2020-07-07 15:15:06 -0700

    "Implemented feed"

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         if (ParseUser.getCurrentUser() != null){
-            goMainActivity();;
+            goMainActivity();
         }
 
         etUsername = findViewById(R.id.etUsername);
